9927bb0b25a3d6a68d36ce2069cc3f578951264e
wkosea <1254843177@qq.com>
2019-03-18 16:03:54 +0800

    update

---
+++ blog_update.sh
@@ -0,0 +1,9 @@
+cd /Users/a1254843177/Desktop/blog/
+git pull
+git add .
+git commit -m "update"
+git push origin master
+ssh -p 2002 root@ZooJeek.cn
+cd /home/anchor/
+git fetch origin master
+git pull origin master
